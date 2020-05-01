f472fae659fe6ca64570312ea29cc9d8fc4d174e
Jesus David Torres <jesu_ti91@hotmail.com>
2020-05-01 17:47:36 -0500

    Refactorizacion

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -29,14 +29,20 @@ public class BoardManager : MonoBehaviour
     {
         for (int x = -1; x < columns + 1; x++)
         {
-            for (int y = -1; y < rows; y++)
+            for (int y = -1; y < rows + 1; y++)
             {
-                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                GameObject toInstantiate = GetRandomInArray(floorTiles);
                 if (x == -1 || y == -1 || x == columns || y == rows)
                 {
-                    toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+                    toInstantiate = GetRandomInArray(outerWallTiles);
                 }
+                Instantiate(toInstantiate, new Vector2(x, y), Quaternion.identity);
             }
         }
+    }
+
+    GameObject GetRandomInArray(GameObject[] array)
+    {
+        return array[Random.Range(0, array.Length)];
     }
 }
