a7e868348d4daa89277d78b6287ace0038480209
Jesus David Torres <jesu_ti91@hotmail.com>
2020-05-01 18:07:53 -0500

    Creando un gameObject dinamico

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -7,6 +7,7 @@ public class BoardManager : MonoBehaviour
 
     public int columns = 8;
     public int rows = 8;
+    private Transform boardHolder;
 
     public GameObject[] floorTiles, outerWallTiles;
 
@@ -27,16 +28,22 @@ public class BoardManager : MonoBehaviour
 
     void BoardSetup()
     {
+        boardHolder = new GameObject("Board").transform;
         for (int x = -1; x < columns + 1; x++)
         {
             for (int y = -1; y < rows + 1; y++)
             {
-                GameObject toInstantiate = GetRandomInArray(floorTiles);
+                GameObject toInstantiate;
                 if (x == -1 || y == -1 || x == columns || y == rows)
                 {
                     toInstantiate = GetRandomInArray(outerWallTiles);
                 }
-                Instantiate(toInstantiate, new Vector2(x, y), Quaternion.identity);
+                else
+                {
+                    toInstantiate = GetRandomInArray(floorTiles);
+                }
+                GameObject instance = Instantiate(toInstantiate, new Vector2(x, y), Quaternion.identity);
+                instance.transform.SetParent(boardHolder);
             }
         }
     }
