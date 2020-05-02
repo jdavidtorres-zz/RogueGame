875d9bc81a416fa280c12b844bb1073e37f74d34
Jesus David Torres <jesu_ti91@hotmail.com>
2020-05-02 18:05:04 -0500

    Clase de movimientos

---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovingObject : MonoBehaviour
+{
+
+    public float moveTime = 0.1f;
+    public LayerMask blockingLayer;
+
+    private float movementSpeed;
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        movementSpeed = 1f / moveTime;
+    }
+
+    protected IEnumerator SmoothMovement(Vector2 end)
+    {
+        float remainingDistace = Vector2.Distance(rigidBody.position, end);
+        while (remainingDistace > float.Epsilon)
+        {
+            Vector2 newPosition = Vector2.MoveTowards(rigidBody.position, end, movementSpeed * Time.deltaTime);
+            rigidBody.MovePosition(newPosition);
+            remainingDistace = Vector2.Distance(rigidBody.position, end);
+            yield return null;
+        }
+    }
+
+    protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+        if (hit.transform == null)
+        {
+            StartCoroutine(SmoothMovement(end));
+            return true;
+        }
+        return false;
+    }
+
+    protected abstract void OnCantMove(GameObject go);
+
+    protected virtual void AttemptMove(int xDir, int yDir)
+    {
+        RaycastHit2D hit;
+        bool canMove = Move(xDir, yDir, out hit);
+        if (canMove)
+        {
+            return;
+        }
+        else
+        {
+            OnCantMove(hit.transform.gameObject);
+        }
+    }
+}
