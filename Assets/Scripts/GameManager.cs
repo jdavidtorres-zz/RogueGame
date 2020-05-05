4d0a50accbdb5bd4858f04754f7c743ea17c81d9
Jesus David Torres <jesu_ti91@hotmail.com>
2020-05-05 17:57:10 -0500

    Agregando co-rutinas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-
-    public BoardManager boardScript;
     public static GameManager instance;
+    public BoardManager boardScript;
+    public float turnDelay = 0.1f;
     public int playerFoodPoints = 100;
-    [HideInInspector] public bool playerTurrn = true;
+    [HideInInspector]
+    public bool playerTurrn = true;
+
+    private List<Enemy> enemies = new List<Enemy>();
+    private bool enemiesMoving;
 
     private void Awake()
     {
@@ -31,16 +36,31 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        enemies.Clear();
         InitGame();
-    }
-
-    void Update()
-    {
-
     }
 
     public void GameOver()
     {
         enabled = false;
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[1].moveTime);
+
+        }
+        playerTurrn = true;
+        enemiesMoving = false;
     }
 }
