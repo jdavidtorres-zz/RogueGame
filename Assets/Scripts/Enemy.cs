0c80b5a5b7f6f2c0af3e732491aeac0622efc831
Jesus David Torres <jesu_ti91@hotmail.com>
2020-05-05 15:15:39 -0500

    Controlando al enemigo

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovingObject
+{
+
+    public int playerDamage;
+
+    private Animator animator;
+    private Transform target;
+    private bool skipMove;
+
+    protected override void Awake()
+    {
+        animator = GetComponent<Animator>();
+        base.Awake();
+    }
+
+    protected override void OnCantMove(GameObject go)
+    {
+        Player hitPlayer = go.GetComponent<Player>();
+        if (hitPlayer != null)
+        {
+            hitPlayer.LoseFood(playerDamage);
+        }
+    }
+
+    protected override void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+    }
+
+    protected override void AttemptMove(int xDir, int yDir)
+    {
+        if (skipMove)
+        {
+            skipMove = false;
+            return;
+        }
+        base.AttemptMove(xDir, yDir);
+        skipMove = true;
+    }
+
+    public void MoveEnemy()
+    {
+        int xDir = 0, yDir = 0;
+        if (Math.Abs(target.position.x - transform.position.x) < float.Epsilon)
+        {
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+        }
+        else
+        {
+            xDir = target.position.x > transform.position.x ? 1 : -1;
+        }
+        AttemptMove(xDir, yDir);
+    }
+
+}
